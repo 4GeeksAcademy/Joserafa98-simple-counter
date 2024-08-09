a9469a1d55a28d61d323d1d8bc86c8994082e21a
Jose Hernandez <JOSERAFAHERNANDEZ.98@GMAIL.COM>
2024-08-09 17:03:55 +0000

    adding the clock

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,7 +9,9 @@ const Home = (props) => {
     return (
         <>
             <div className="row bg-secondary justify-content-center">
-                <div className="clockIcon"><i className="fa-regular fa-clock"></i></div>
+                <div className="clockIcon"> 
+                    <svg width={15} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120l0 136c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2 280 120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
+                </div>
                 <div className="digitSix">{props.digitSix}</div>
                 <div className="digitFive">{props.digitFive}</div>
                 <div className="digitFour">{props.digitFour}</div>
